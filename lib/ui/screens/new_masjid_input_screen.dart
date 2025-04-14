f3e53c91b9b84f551eba9e9f152b12231a2fd8db
fadelfffar <fadel.farinsqi1739@gmail.com>
2025-04-14 10:56:32 +0700

    add masjid name form that insert data to supabase on new masjid screen

--- lib/ui/screens/new_masjid_input_screen.dart
+++ lib/ui/screens/new_masjid_input_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class NewMasjidInputScreen extends StatefulWidget {
   const NewMasjidInputScreen({super.key});
@@ -10,6 +11,27 @@ class NewMasjidInputScreen extends StatefulWidget {
 class _NewMasjidInputScreenState extends State<NewMasjidInputScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(height: 80,),
+          Form(
+            child: Column(
+              children: [
+                Text("Add New Masjid"),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'ID'),
+                  initialValue: "Masjid Name",
+                  onFieldSubmitted: (value) async {
+                    await Supabase.instance.client
+                      .from('masjid')
+                      .insert({'masjid_name': value});
+                  },
+                ),
+              ],
+            ),)
+        ],
+      ),
+    );
   }
 }
