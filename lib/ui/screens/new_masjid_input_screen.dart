032f190d03ea8c374db3a359595441c2b282466c
fadelfffar <fadel.farinsqi1739@gmail.com>
2025-04-14 11:16:59 +0700

    add masjid name, city form that insert data to supabase on new masjid screen and prepare for temporary form data

--- lib/ui/screens/new_masjid_input_screen.dart
+++ lib/ui/screens/new_masjid_input_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:for_fajr/ui/ui_widgets/primary_button.dart';
+import 'package:native_geofence/native_geofence.dart';
 
 class NewMasjidInputScreen extends StatefulWidget {
   const NewMasjidInputScreen({super.key});
@@ -9,28 +10,54 @@ class NewMasjidInputScreen extends StatefulWidget {
 }
 
 class _NewMasjidInputScreenState extends State<NewMasjidInputScreen> {
+
+  //TODO: add Masjid Class that specifies Supabase Data
+  late /* Masjid */ data;
+
+  @override
+  void initState() {
+    //TODO: make temporary data value to store TextForm
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(height: 80,),
-          Form(
-            child: Column(
-              children: [
-                Text("Add New Masjid"),
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'ID'),
-                  initialValue: "Masjid Name",
-                  onFieldSubmitted: (value) async {
-                    await Supabase.instance.client
-                      .from('masjid')
-                      .insert({'masjid_name': value});
-                  },
-                ),
-              ],
-            ),)
-        ],
+      body: Container(
+        padding: EdgeInsets.only(left: 24, right: 24),
+        child: Column(
+          children: [
+            SizedBox(height: 80,),
+            Form(
+              child: Column(
+                children: [
+                  Text("Add New Masjid"),
+                  // TextFormField(
+                  //   decoration: InputDecoration(labelText: 'Masjid Name'),
+                  //   onFieldSubmitted: (value) async {
+                  //     await Supabase.instance.client
+                  //       .from('masjid')
+                  //       .insert({'masjid_name': value});
+                  //   },
+                  // ),
+                  // TextFormField(
+                  //   decoration: InputDecoration(labelText: 'City'),
+                  //   onFieldSubmitted: (value) async {
+                  //     await Supabase.instance.client
+                  //       .from('masjid')
+                  //       .insert({'location_city': value});
+                  //   },
+                  // ),
+                  //TODO: add a button that submit all data in one go
+                  PrimaryButton(
+                    onPressedData: () async{
+                      if (condition) {
+                        
+                      }
+                    }, textButtonData: 'Register')
+                ],
+              ),)
+          ],
+        ),
       ),
     );
   }
