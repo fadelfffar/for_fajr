2941cceac94fc4771d1b22e0830c0bfc051c86ee
fadelfffar <fadel.farinsqi1739@gmail.com>
2025-04-14 10:44:14 +0700

    add masjid name form on new masjid screen

---
+++ lib/ui/screens/new_masjid_input_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NewMasjidInputScreen extends StatefulWidget {
+  const NewMasjidInputScreen({super.key});
+
+  @override
+  State<NewMasjidInputScreen> createState() => _NewMasjidInputScreenState();
+}
+
+class _NewMasjidInputScreenState extends State<NewMasjidInputScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
